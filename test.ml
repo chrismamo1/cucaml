61fa1f73f0b5da9926c54ab07e73f92ad7df0dfd
chrismamo1 <christophermcalpine@gmail.com>
2017-11-01 20:36:01 -0600

    fix example

--- test.ml
+++ test.ml
@@ -3,7 +3,7 @@ let () =
   let arr = Array.init 20_000_000 (fun _ -> Random.float 100.0) in
   let arr1 = Array.copy arr in
   let arr2 = Array.copy arr in
-  let kSrc = CudaArray.generateKernel "(if (>= x 1.99999) (* x x) (+ x 1.0))" in
+  let kSrc = CudaArray.generateKernel "(if (>= x 1.99999) (* x x) (* (* x 1.1) (* x 1.1)))" in
   Printf.printf "kSrc:\n%s\n" kSrc;
   let cuStart = Unix.gettimeofday() in
   let () =
@@ -16,7 +16,7 @@ let () =
   let seStart = Unix.gettimeofday() in
   let () =
     Array.iteri
-      (fun i x -> if x >= 1.99999 then arr.(i) <- x *. x else arr.(i) <- x +. 1.0)
+      (fun i x -> if x >= 1.99999 then arr.(i) <- x *. x else arr.(i) <- (x *. 1.1) *. (x *. 1.1))
       arr2
   in
   let seEnd = Unix.gettimeofday() in
