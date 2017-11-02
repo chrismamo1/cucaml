2acf6189bf5ebd67908c6779fe50235c4109c90e
chrismamo1 <christophermcalpine@gmail.com>
2017-11-01 19:54:16 -0600

    fix example

--- test.ml
+++ test.ml
@@ -1,6 +1,6 @@
 let () =
   let arr = [| 1.0; 2.0; 3.0 |] in
-  let kSrc = CudaArray.generateKernel "(+ x (if (= 1.0 x) 2 x))" in
+  let kSrc = CudaArray.generateKernel "(+ x (if (= 1.0 x) (+ 2.0 0.0) (+ x 0.0)))" in
   Printf.printf "kSrc:\n%s\n" kSrc;
   let () =
     CudaArray.mapFloatArrayInPlace
